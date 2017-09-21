50df6a39098ea82217f5fbec59c91258e9379c0f
hecheng <cheryl.he@softgridinc.com>
2017-09-21 11:46:24 +0800

    commit second

--- src/test/test.java
+++ src/test/test.java
@@ -2,12 +2,17 @@ package test;
 
 import javax.swing.text.AbstractDocument.Content;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 public class test {
 	 public static void main(String[] args) {
 		 String YP_content = "�������#code#,����Ǳ��˲����������";
 		 String Content = YP_content.replace("#code#", "126456");
 		 
 		 System.out.println(Content);
-	
+	 
 }
+	 public String get() {		 
+		 return "hello world";
+	 }
 }
